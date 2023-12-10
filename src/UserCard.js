1bef6d3595ba197b769df6cfbb624a120f092372
pramod <pramodacharya808@gmail.com>
2023-12-10 13:28:14 +0530

    initial commit

---
+++ src/UserCard.js
@@ -0,0 +1,9 @@
+import React, {useState} from "react";
+import userImg from "./Assets/mydp.jpg";
+import * as FaIcons from "react-icons/fa";
+
+function UserCard(){
+    return <div> Hello World</div>
+}
+
+export default UserCard;
